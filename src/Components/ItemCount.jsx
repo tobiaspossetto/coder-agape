529704f9d5e7ccdae75a97919a9de403f9646da3
tobiaspossetto <tobigpossetto@gmail.com>
2021-07-09 12:19:25 -0300

    Clase de map y asincronia

--- src/Components/ItemCount.jsx
+++ src/Components/ItemCount.jsx
@@ -1,8 +1,62 @@
-import React , {useState} from 'react'
+import React, { useState } from 'react'
 
 const ItemCount = (props) => {
 
     const [valor, setValor] = useState(props.initial);
+    
+
+    
+    // [
+//     {
+//         "id": 1,
+//         "name": "Remera Blanca",
+//         "precio": 1450,
+//         "descripcion": "Remera blanca para hombre",
+//         "categoria": "hombre",
+//         "stock": 4
+//     },
+//     {
+//         "id": 2,
+//         "name": "Remera azul",
+//         "precio": 1450,
+//         "descripcion": "Remera azul para hombre",
+//         "categoria": "hombre",
+//         "stock": 4
+//     },
+//     {
+//         "id": 3,
+//         "name": "Remera rosa",
+//         "precio": 1450,
+//         "descripcion": "Remera rosa para mujer",
+//         "categoria": "mujer",
+//         "stock": 4
+//     },
+//     {
+//         "id": 4,
+//         "name": "Remera lila",
+//         "precio": 1450,
+//         "descripcion": "Remera lila para mujer",
+//         "categoria": "mujer",
+//         "stock": 4
+//     },
+//     {
+//         "id": 5,
+//         "name": "Remera negra",
+//         "precio": 1450,
+//         "descripcion": "Remera negra para hombre",
+//         "categoria": "hombre",
+//         "stock": 4
+//     },
+//     {
+//         "id": 6,
+//         "name": "Remera verde",
+//         "precio": 1450,
+//         "descripcion": "Remera verde para hombre",
+//         "categoria": "hombre",
+//         "stock": 4
+//     }
+// ]
+
 
     const resta = () => {
         if(valor>1){
